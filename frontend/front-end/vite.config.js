19ece4daf7ec9a24d8dd1722afc65313da06ad87
Yafei Deng <weihaianni@gmail.com>
2024-07-11 23:51:38 +1000

    front end added (vite is used)

---
+++ frontend/front-end/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import path from 'path';
+
+export default defineConfig({
+    server:{
+        port: 8800,
+    },
+    resolve:{
+        alias:{
+            "@":path.resolve(__dirname,'/src')
+        }
+
+    }
+
+})
